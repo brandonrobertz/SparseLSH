48241031453d5a0d07d0c24d8e5974cdec13d24f
Brandon Roberts <brandon@bxroberts.org>
2022-06-18 17:37:38 -0700

    Add scikit-learn to requirements

--- setup.py
+++ setup.py
@@ -15,11 +15,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url='https://github.com/brandonrobertz/sparselsh',
     download_url='https://github.com/brandonrobertz/SparseLSH/releases/tag/v2.1.0',
-    keywords = ['clustering', 'sparse', 'lsh'],
-    packages = setuptools.find_packages(),
+    keywords=['clustering', 'sparse', 'lsh'],
+    packages=setuptools.find_packages(),
     install_requires=[
         'numpy>=1.18.4,<2.0',
-        'scipy>=1.4.1,<2.0'
+        'scipy>=1.4.1,<2.0',
+        'scikit-learn>=0.20,<1.0'
     ],
     classifiers=[
         'Intended Audience :: Developers',

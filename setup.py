e257d9a7dcff5bc85c9ae124ef983a2b793e9f87
Brandon Roberts <brandon@bxroberts.org>
2024-06-04 17:32:15 -0700

    Move to setup.cfg, add pyproject

--- setup.py
+++ setup.py
@@ -1,41 +1,23 @@
+import codecs
+import os.path
 import setuptools
 
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
 
-setuptools.setup(
-    name='sparselsh',
-    version='2.2.0',
-    author='Brandon Roberts',
-    author_email='brandon@bxroberts.org',
-    description='A locality sensitive hashing library with an emphasis on large, sparse datasets.',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/brandonrobertz/sparselsh',
-    download_url='https://github.com/brandonrobertz/SparseLSH/releases/tag/v2.2.0',
-    keywords=['clustering', 'sparse', 'lsh'],
-    packages=setuptools.find_packages(),
-    install_requires=[
-        'numpy>=1.18.6,<2.0',
-        'scipy>=1.0.0,<2.0',
-        'scikit-learn>=0.24.0,<2.0'
-    ],
-    extras_require={
-        "test": ["pytest"],
-        "redis": ["redis>=2.10.1,<3.0"]
-    },
-    entry_points="""
-        [console_scripts]
-        sparselsh=sparselsh.cli:cli
-    """,
-    classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Topic :: Software Development :: Libraries',
-        ],
-)
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+setuptools.setup()
